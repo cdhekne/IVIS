ced76d2e2376700db0307173d69e2c46613ce670
Chinmay Dhekne <chinmaydhekne@gmail.com>
2015-04-13 20:05:31 -0700

    Dummy Issue Commit
    Heiinnn

--- app/src/main/java/edu/asu/msse/cdhekne/ivis/LandingPage.java
+++ app/src/main/java/edu/asu/msse/cdhekne/ivis/LandingPage.java
@@ -12,6 +12,7 @@ public class LandingPage extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing_page);
+        System.out.print("Dummy Issue");
 
     }
 
