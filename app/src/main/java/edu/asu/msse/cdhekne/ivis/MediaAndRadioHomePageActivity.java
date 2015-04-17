bb25c49f8ca8170962265852064ee38c720cdc40
jns22592 <japa22swadia@gmail.com>
2015-04-16 22:08:57 -0700

    Media and Radio homepage

--- app/src/main/java/edu/asu/msse/cdhekne/ivis/MediaAndRadioHomePageActivity.java
+++ app/src/main/java/edu/asu/msse/cdhekne/ivis/MediaAndRadioHomePageActivity.java
@@ -1,11 +1,9 @@
 package edu.asu.msse.cdhekne.ivis;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,19 +13,23 @@ import android.widget.Toast;
 import java.util.List;
 
 
-public class LandingPage extends ActionBarActivity implements View.OnClickListener {
+public class MediaAndRadioHomePageActivity extends ActionBarActivity implements View.OnClickListener {
 
     private ImageButton mic_button;
+    private ImageButton home_button;
     private static final int SPEECH_REQUEST_CODE = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
-//        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        setContentView(R.layout.activity_radio_and_media_home_page);
         mic_button =(ImageButton)findViewById(R.id.mic_button);
         mic_button.setOnClickListener(this);
 
+        home_button = (ImageButton)findViewById(R.id.home_button);
+        home_button.setOnClickListener(this);
     }
+
 
     @Override
     public void onClick(View v){
@@ -35,6 +37,12 @@ public class LandingPage extends ActionBarActivity implements View.OnClickListen
             Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
             intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
             startActivityForResult(intent, SPEECH_REQUEST_CODE);
+        }
+
+        if(v.getId() == home_button.getId()){
+
+            Intent intent = new Intent(MediaAndRadioHomePageActivity.this, LandingPage.class);
+            startActivity(intent);
         }
     }
 
@@ -50,16 +58,16 @@ public class LandingPage extends ActionBarActivity implements View.OnClickListen
 
                 /*Intent goToNavigationIntent = new Intent();
                 startActivity(goToNavigationIntent);*/
-                Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), spokenText, Toast.LENGTH_SHORT).show();
             }
             else if(spokenText.contains("media")){
-                Intent goToMediaIntent = new Intent(LandingPage.this, MediaAndRadioHomePageActivity.class);
-                startActivity(goToMediaIntent);
+                /*Intent goToMediaIntent = new Intent();
+                startActivity(goToMediaIntent);*/
                 Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
             }
             else if(spokenText.contains("phone")|| spokenText.contains("call")){
-                Intent goToPhoneIntent = new Intent(LandingPage.this, CallAndContactsHomePageActivity.class);
-                startActivity(goToPhoneIntent);
+                /*Intent goToPhoneIntent = new Intent();
+                startActivity(goToPhoneIntent);*/
                 Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
             }
             else if(spokenText.contains("controls")){
@@ -81,10 +89,12 @@ public class LandingPage extends ActionBarActivity implements View.OnClickListen
         super.onActivityResult(requestCode, resultCode, data);
     }
 
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_landing_page, menu);
+        getMenuInflater().inflate(R.menu.menu_radio_and_media_home_page, menu);
         return true;
     }
 
@@ -102,4 +112,7 @@ public class LandingPage extends ActionBarActivity implements View.OnClickListen
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
 }
