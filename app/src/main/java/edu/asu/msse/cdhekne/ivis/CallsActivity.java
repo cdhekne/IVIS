802b3e13d9048bf7b9917fc04a2bff33860f3c56
Avani Chandurkar <ajchandu@asu.edu>
2015-04-17 20:10:02 -0700

    Calls MainPage
    Everything done with voice framework

--- app/src/main/java/edu/asu/msse/cdhekne/ivis/CallsActivity.java
+++ app/src/main/java/edu/asu/msse/cdhekne/ivis/CallsActivity.java
@@ -1,18 +1,164 @@
 package edu.asu.msse.cdhekne.ivis;
 
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
-public class CallsActivity extends ActionBarActivity {
+public class CallsActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private ImageButton mic_button;
+    private ImageButton back_button;
+    private ImageButton one, two, three, four, five, six, seven, eight, nine, asterisk, hash, zero;
+    private TextView mainTV;
+    private static final int SPEECH_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calls);
+
+        mic_button =(ImageButton)findViewById(R.id.mic_button);
+        mic_button.setOnClickListener(this);
+
+        back_button = (ImageButton)findViewById(R.id.back_button);
+        back_button.setOnClickListener(this);
+
+        mainTV = (TextView)findViewById(R.id.mainTV);
+
+        one = (ImageButton)findViewById(R.id.button_one);
+        one.setOnClickListener(this);
+        two = (ImageButton)findViewById(R.id.button_two);
+        two.setOnClickListener(this);
+        three = (ImageButton)findViewById(R.id.button_three);
+        three.setOnClickListener(this);
+        four = (ImageButton)findViewById(R.id.button_four);
+        four.setOnClickListener(this);
+        five = (ImageButton)findViewById(R.id.button_five);
+        five.setOnClickListener(this);
+        six = (ImageButton)findViewById(R.id.button_six);
+        six.setOnClickListener(this);
+        seven = (ImageButton)findViewById(R.id.button_seven);
+        seven.setOnClickListener(this);
+        eight = (ImageButton)findViewById(R.id.button_eight);
+        eight.setOnClickListener(this);
+        nine = (ImageButton)findViewById(R.id.button_nine);
+        nine.setOnClickListener(this);
+        asterisk = (ImageButton)findViewById(R.id.button_asterisk);
+        asterisk.setOnClickListener(this);
+        hash = (ImageButton)findViewById(R.id.button_hash);
+        hash.setOnClickListener(this);
+        zero = (ImageButton)findViewById(R.id.button_zero);
+        zero.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        if(v.getId()==mic_button.getId()){
+            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            startActivityForResult(intent, SPEECH_REQUEST_CODE);
+        }
+
+        if(v.getId() == back_button.getId()){
+            Intent intent = new Intent(CallsActivity.this, CallAndContactsHomePageActivity.class);
+            startActivity(intent);
+        }
+
+        if(v.getId() == one.getId()){
+            mainTV.append("1");
+        }
+        if(v.getId() == two.getId()){
+            mainTV.append("2");
+        }
+        if(v.getId() == three.getId()){
+            mainTV.append("3");
+        }
+        if(v.getId() == four.getId()){
+            mainTV.append("4");
+        }
+        if(v.getId() == five.getId()){
+            mainTV.append("5");
+        }
+        if(v.getId() == six.getId()){
+            mainTV.append("6");
+        }
+        if(v.getId() == seven.getId()){
+            mainTV.append("7");
+        }
+        if(v.getId() == eight.getId()){
+            mainTV.append("8");
+        }
+        if(v.getId() == nine.getId()){
+            mainTV.append("9");
+        }
+        if(v.getId() == zero.getId()){
+            mainTV.append("0");
+        }
+        if(v.getId() == asterisk.getId()){
+            mainTV.append("*");
+        }
+        if(v.getId() == hash.getId()){
+            mainTV.append("#");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+//            Log.d("Speech Output:---->>>>>",spokenText);
+            if(spokenText.contains("navigate")){
+
+                /*Intent goToNavigationIntent = new Intent();
+                startActivity(goToNavigationIntent);*/
+                Toast.makeText(getApplicationContext(), spokenText, Toast.LENGTH_SHORT).show();
+            }
+            else if(spokenText.contains("media")){
+                /*Intent goToMediaIntent = new Intent();
+                startActivity(goToMediaIntent);*/
+                Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+            }
+            else if(spokenText.contains("phone")|| spokenText.contains("call")){
+                /*Intent goToPhoneIntent = new Intent();
+                startActivity(goToPhoneIntent);*/
+                Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+            }
+            else if(spokenText.contains("controls")){
+                /*Intent goToControlsIntent = new Intent();
+                startActivity(goToControlsIntent);*/
+                Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+            }
+            else if(spokenText.contains("climate")){
+                /*Intent goToClimateIntent = new Intent();
+                startActivity(goToClimateIntent);*/
+                Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+            }
+            else if(spokenText.contains("apps")){
+                /*Intent goToAppsIntent = new Intent();
+                startActivity(goToAppsIntent);*/
+                Toast.makeText(getApplicationContext(),spokenText,Toast.LENGTH_SHORT).show();
+            }
+            else if(spokenText.contains("home")){
+                Intent goToHomeIntent = new Intent(CallsActivity.this, LandingPage.class);
+                startActivity(goToHomeIntent);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
 
 
     @Override
